225b9abc512c48bdc24b8cf78ffdbb0d4f55a686
dinislam146007 <guitarp146007@gmail.com>
2024-11-12 22:58:36 +0500

    Добавлен venv в .gitignore

--- basic/handlers.py
+++ basic/handlers.py
@@ -16,7 +16,7 @@ async def send_welcome(message: Message):
         InlineKeyboardButton(text='СГР', callback_data='cert СГР')],
         [InlineKeyboardButton(text='Связь с менежером', url='https://t.me/Nastia_NZ')]
     ]
-    await message.answer("Привет. Это бот сертификации. Напиши что хочешь"
+    await message.answer("Привет. Это бот сертификации"
     ,reply_markup=InlineKeyboardMarkup(inline_keyboard=kb)
                         )
 
