719e3e3c87602e375b8b894549d84032f55f4bf8
dinislam146007 <guitarp146007@gmail.com>
2024-11-13 19:54:03 +0500

    sertifikat add

--- basic/handlers.py
+++ basic/handlers.py
@@ -2,34 +2,45 @@ from aiogram import Router, F
 from aiogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
 from aiogram.filters import Command
 from search.search import find_certificates_for_product
+from keyboard.inline import *
+from aiogram.fsm.context import FSMContext, State, StatesGroup
 
 
 router = Router()
 
+class SearchState(StatesGroup):
+    text = State()
+
+    
 @router.message(Command("start"))
 async def send_welcome(message: Message):
-    kb = [
-        [InlineKeyboardButton(text='Проверить продукцию', callback_data='search')],
-        [InlineKeyboardButton(text='Сертификат ТРТС', callback_data='cert strtc'),
-        InlineKeyboardButton(text='Декларация ТРТС', callback_data='cert dtrtc')],
-        [InlineKeyboardButton(text='Сертификат ГОСТр', callback_data='cert gostr'),
-        InlineKeyboardButton(text='СГР', callback_data='cert sgr')],
-        [InlineKeyboardButton(text='Связь с менежером', url='https://t.me/Nastia_NZ')]
-    ]
-    await message.answer("Привет"
-    ,reply_markup=InlineKeyboardMarkup(inline_keyboard=kb)
-                        )
+    await message.answer(
+        text="Привет",
+        reply_markup=start_inline()  
+    )
+                        
+@router.callback_query(F.data == 'close_state')
+async def close_state(callback: CallbackQuery, state: FSMContext):
+    current_state = await state.get_state()
+    if current_state is None:
+        return
+    await state.clear()
+    await callback.answer("Ожидание ответа успешно отменено.")
+    await callback.message.edit_text(
+        text="Привет",
+        reply_markup=start_inline()  
+    )
 
+@router.callback_query(F.data == 'search')
+async def search(callback: CallbackQuery, state: FSMContext):
+    msg = await callback.message.edit_text(
+        text='Введите названи е вашей продукции',
+        reply_markup=close_state()
+    )
+    await state.set_state(SearchState.text)
 
-@router.callback_query(F.data.startswith('cert'))
-async def cert(callback: CallbackQuery):
-    cert_info = callback.data.split()[1]
-    await callback.message.edit_text(text=f'Сертификат: ')
-
-
-
-@router.message()
-async def send_certificate(message: Message):
+@router.message(SearchState.text)
+async def search_state(message: Message, state: FSMContext):
     product_name = message.text.strip().lower()
     certificates = find_certificates_for_product(product_name)
 
@@ -53,3 +64,16 @@ async def send_certificate(message: Message):
         msg = "Тип сертификата для этого товара не найден."
         await message.answer(msg)
 
+
+@router.callback_query(F.data.startswith('cert'))
+async def cert(callback: CallbackQuery):
+    cert_info = int(callback.data.split()[1])
+    if cert_info == 0:
+        text = 'Сертификат ТРТС'
+    elif cert_info == 1:
+        text = 'Декларация ТРТС'
+    elif cert_info == 2:
+        text = 'Сертификат ГОСТр'
+    else:
+        text = 'СГР'
+    await callback.message.edit_text(text=f'Сертификат: {text}')
