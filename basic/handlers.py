f08ff88aa0f04908fc6b8c0bae41977226b2a177
dinislam146007 <guitarp146007@gmail.com>
2024-11-15 19:20:46 +0500

    venv

--- basic/handlers.py
+++ basic/handlers.py
@@ -147,10 +147,8 @@ async def choice_request(callback: CallbackQuery, state: FSMContext,bot: Bot):
     action = callback.data.split()[1]
     data = await state.get_data()
     if action == 'blank':
-        await callback.message.answer(f'{doc}')
-        await bot.send_document(
-            callback.from_user.id,
-            ('Заявка.docx', doc)
+        await callback.message.reply_document(
+            open(doc, 'rb')
         )
         await bot.delete_message(message_id=data['msg'], chat_id=callback.from_user.id)
     else:
